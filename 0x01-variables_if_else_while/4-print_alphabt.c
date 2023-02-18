c108898bb630d6e5724d77a449828c7818fa0225
sleekyucee <uchesy4u@yahoo.com>
2023-02-17 21:28:07 -0800

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - The entry point of C programs
+* @void: The function takes no parameters
+*
+* Description: This program prints the alphabet in lowercase,
+* except 'q' and 'e' followed by a new line. You are to use only
+* the putchar function not more than twice.
+* Return: 0
+*/
+int main(void)
+{
+	char not_complete = 'a';
+
+	while (not_complete <= 'z')
+	{
+		if (not_complete != 'e' && not_complete != 'q')
+			putchar(not_complete);
+		not_complete++;
+	}
+	putchar('\n');
+	return (0);
+}
