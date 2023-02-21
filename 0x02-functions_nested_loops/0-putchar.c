77e95543d2733142f21e520497c18d26b03cacc5
sleekyucee <uchesy4u@yahoo.com>
2023-02-21 13:18:31 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
 #include "main.h"
-#include <unistd.h>
 /**
- * _putchar - This writes the character c to stdout
- * @c: The character to print
- *
- * Return: Always 1 (success)
- * Error: -1 is returned, and error number is set appropriately
- */
-int _putchar(char c)
+* main - print _putchar
+* is written by mbah
+* Return: 0
+*/
+
+int main(void)
 {
-	return (write(1, &c, 1));
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
