891953db06104fcd189bb011223131e379675f8d
sleekyucee <uchesy4u@yahoo.com>
2023-02-21 23:29:50 -0800

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,5 @@
+MAIN.H CODE
+
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
@@ -8,4 +10,3 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 #endif
-
