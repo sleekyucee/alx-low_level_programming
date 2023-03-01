ddcb1f781c5f7eea3de77988b0cfa688e8ee474d
sleekyucee <uchesy4u@yahoo.com>
2023-03-01 11:44:59 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - this describes the main function
+* main - magic
 *
 * Return: 0
 */
@@ -24,6 +24,6 @@ p = &n;
 */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
-printf("&p[2] = %d\n",&p[2]);
+printf("a[2] = %d\n",a[2]);
 return (0);
 }
