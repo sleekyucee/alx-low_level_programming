34661ab0d01eebc225610c11f8925fd46c3758aa
sleekyucee <uchesy4u@yahoo.com>
2023-03-14 00:59:00 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,6 @@
 * @s2: input two to concat
 * Return: concat of s1 and s2
 */
-
 char *str_concat(char *s1, char *s2)
 {
 
@@ -26,19 +25,16 @@ char *str_concat(char *s1, char *s2)
 
 	while (s2[ci] != '\0')
 		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
 
+	conct = malloc(sizeof(char) * (i + ci + 1));
 	if (conct == NULL)
 		return (NULL);
-
 	i = ci = 0;
-
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
 		i++;
 	}
-
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
