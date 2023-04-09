28044de687d38b0f64ca9c5373b399087c224301
Uchechukwu <uchesy4u@yahoo.com>
2023-04-09 14:22:06 -0700

    1-create_file.c updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,26 +19,19 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_WRONLY | O_CREAT | 0_TRUNC, 0600);
-	if (fd == -1)
-	{
-		return (-1);
-	}
-
 	if (text_content != NULL)
 	{
-		while (text_content[len] != '\0')
-		{
+		for (len = 0; text_content[len];)
 			len++;
-		}
-		bytes-written = write(fd, text_content, len);
 	}
 
-	if (bytes_written ==  -1 || (text_content != NULL && ret != len))
+	fd = open(filename, O_WRONLY | O_CREAT | 0_TRUNC, 0600);
+	bytes_written = write(fd, text_content, len);
+
+	if (fd == -1 || bytes_written == -1)
 	{
 		return (-1);
 	}
-
 	close(fd);
 	return (1);
 }
