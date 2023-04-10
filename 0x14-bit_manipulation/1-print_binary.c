968db9978428dc313be19efc332eebc9c31fcf10
Uchechukwu <uchesy4u@yahoo.com>
2023-04-10 12:41:39 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,23 +6,11 @@
  *
  * Return: void
  */
+
 void print_binary(unsigned long int n)
 {
-	int i, counts = 0;
-	unsigned long int printed;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	for (i = 63; i >= 0; i--)
-	{
-		printed = n >> i;
-
-		if (printed & 1)
-		{
-			_putchar('1');
-			counts++;
-		}
-		else if (counts)
-			_putchar('0');
-	}
-	if (!counts)
-		_putchar('0');
+	putchar((n & 1) + '0');
 }
