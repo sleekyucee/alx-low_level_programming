5ae3cc677b159c9c404a4866e82a3ca8cc312c62
Uchechukwu <uchesy4u@yahoo.com>
2023-04-10 12:52:14 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,13 +4,23 @@
  * print_binary - prints the binary representation of a number
  * @n: unsigned long int number to print in binary
  *
- * Return: void
+ * Return: converted number
  */
 
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
+	{
 		print_binary(n >> 1);
+	}
 
-	putchar((n & 1) + '0');
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+
+	else
+	{
+		_putchar('0');
+	}
 }
