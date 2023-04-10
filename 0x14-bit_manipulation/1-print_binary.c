0b9840e1f07ab38d91bd678e10dbc0c2dead7fdf
Uchechukwu <uchesy4u@yahoo.com>
2023-04-10 04:19:30 -0700

    1-print_binary.c updated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,25 +10,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = ULONG_MAX; /* mask with all bits set to 1 */
-	int len = sizeof(unsigned long int) * 8; /* number of bits in n */
-	/**
-	 * Shift the mask to the left until the leftmost
-	 * 1 is in the correct position
-	 */
-	while ((mask & n) == 0 && len > 1)
-	{
-		mask >>= 1;
-		len--;
-	}
-
-	/* Print the binary representation of n */
-	while (mask)
-	{
-		if (n & mask)
-			putchar('1');
-		else
-			putchar('0');
-		mask >>= 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	putchar((n & 1) + '0');
 }
