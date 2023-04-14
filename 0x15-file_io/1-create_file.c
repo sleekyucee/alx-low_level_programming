36aa4e1e0fdbf5047e4cd2cc114bea057e9b1a46
Uchechukwu <uchesy4u@yahoo.com>
2023-04-14 10:08:21 -0700

    1-create_file.c updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,28 +13,25 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, bytes_written, len = 0;
 
-	if (filename == NULL)
-		return (-1);
+	char empty_string[] = "";
+
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content != NULL && text_content[0] != '\0')
 	{
 		while (text_content[len] != '\0')
 		{
 			len++;
 		}
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
-
-	bytes_written = write(fd, text_content, len);
-
-	if (bytes_written == -1)
-	{
-		close(fd);
-		return (-1);
-	}
-
 	close(fd);
-	return (0);
+	return (1);
 }
