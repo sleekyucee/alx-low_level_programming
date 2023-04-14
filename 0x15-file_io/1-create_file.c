0e856e59351a8de4e7cf923235a55b869bac711c
Uchechukwu <uchesy4u@yahoo.com>
2023-04-14 09:29:09 -0700

    1-create_file.c modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,29 +3,34 @@
 /**
  * create_file - creates a new file with specified content
  * @filename: the name of the file to create
- * @text_content: a NULL-terminated string to write to the file
+ * @textContent: a NULL-terminated string to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
 
-int create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *textContent)
 {
 	int fd, bytes_written, len = 0;
+
+	fd = open(filename, O_WRONLY | O_CREAT | 0_TRUNC, 0600);
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
+	if (textContent != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		textContent = "";
 	}
+	while (textContent[len] != '\0')
+	{
+		len++
+	}
+	bytes_written = write(fd, textContent, len);
 
-	fd = open(filename, O_WRONLY | O_CREAT | 0_TRUNC, 0600);
-	bytes_written = write(fd, text_content, len);
-
-	if (fd == -1 || bytes_written == -1)
+	if (bytes_written == -1)
+	{
 		return (-1);
+	}
 
 	close(fd);
 	return (1);
