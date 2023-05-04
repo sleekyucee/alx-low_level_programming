2c671aa0393c3008ee562690558ea9eddd8ed5f6
Uchechukwu Amua <uchesy4u@yahoo.com>
2023-05-04 15:49:46 -0700

    0-binary_to_uint.c updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,34 +10,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0, result = 0;
+	unsigned int result = 0;
 
 	if (!b)
-	{
 		return (0);
-	}
 
-	while (b[i] != '\0')
+	while (*b != '\0')
 	{
-		if (b[i] != '0' && b[i] != '1')
-		{
+		if (*b != '0' && *b != '1')
 			return (0);
-		}
-		i++
-	}
 
-	i = 0;
-
-	while (b[i] != '\0')
-	{
-		result <<= 1;
-
-		if (b[i] & 1)
-		{
-			result += 1;
-		}
-
-		i++
+		result = result * 2 + (*b - '0');
+		b++;
 	}
 
 	return (result);
