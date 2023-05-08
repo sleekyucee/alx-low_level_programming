559a2bbb3686516f03ee4b4700e1a0a7923c016c
Uchechukwu Amua <uchesy4u@yahoo.com>
2023-05-08 12:42:12 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 /**
  * create_file - creates a new file with specified content
  * @filename: the name of the file to create
@@ -15,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
@@ -33,5 +35,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	close(fd);
-	return (0);
+	return (1);
 }
